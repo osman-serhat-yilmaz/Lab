f6079bfc7cc90bb02263419733c40074c1e9d530
osman-serhat-yilmaz <serhat.yilmaz@ug.bilkent.edu.tr>
2021-10-20 15:35:57 +0300

    implement findAll method

--- src/StudentSearch.java
+++ src/StudentSearch.java
@@ -11,4 +11,26 @@ public class StudentSearch {
 
         return false;
     }
-}
+	
+	public Student findOne(ArrayList<Student> students, String name) throws Exception {
+		for (Student student: students)
+			if (student.getName().equals(name))
+				return student;
+			
+		throw new Exception("There is no student with the given name!");
+	}
+	
+	public ArrayList<Student> findAll(ArrayList<Student> students, String name) throws Exception {
+		ArrayList<Student> result = new ArrayList<Student>();
+		for (Student student: students)
+			if (student.getName().equals(name)) {
+				result.add(student);
+			}
+			
+		if (result.isEmpty()) {
+			throw new Exception("There is no student with the given name!");
+		}
+		return result;
+	}
+
+}	
